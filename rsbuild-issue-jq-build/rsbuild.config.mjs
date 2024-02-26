f36fef5d1336a672eda3e4e123d36cbdfdba16b1
Zhanghao <yukiniro@hotmail.com>
2024-02-26 18:11:36 +0800

    chore: update rsbuild-issue

--- rsbuild-issue-jq-build/rsbuild.config.mjs
+++ rsbuild-issue-jq-build/rsbuild.config.mjs
@@ -1,6 +1,19 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import minifyPlugin from '@rspack/plugin-minify'
 
 export default defineConfig({
   plugins: [pluginReact()],
+  tools: {
+    rspack: {
+      optimization: {
+        minimize: true,
+        minimizer: [
+          new minifyPlugin({
+            minifier: 'terser',
+          }),
+        ],
+      },
+    },
+  },
 });
