d19e9f41da7834991d9abc06de9824d835fdb1d1
Zhanghao <yukiniro@hotmail.com>
2024-07-08 17:50:31 +0800

    feat: add combobox reproduce

---
+++ shadcn-combobox-error/src/App.tsx
@@ -0,0 +1,12 @@
+import { ComboboxDemo } from "@/components/ui/combobox-demo";
+
+
+function App() {
+  return (
+    <div className="w-full h-full">
+      <ComboboxDemo />
+    </div>
+  );
+}
+
+export default App;
